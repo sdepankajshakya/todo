f2217d3035513ced6e1944f7f41a63866a2a58e0
Pankaj Shakya <pankaj@metadesignsolutions.co.uk>
2025-02-23 10:15:50 +0545

    feat: add service worker for instant updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'mytodolist';
+
+  constructor(private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(() => {
+        console.log('New version available! Reloading...');
+        window.location.reload(); // Force refresh
+      });
+    }
+  }
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/ngsw-worker.js').then(() => {
+        console.log('Service Worker Registered');
+      });
+    });
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
